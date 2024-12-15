92654662097bdba99a948d026fea33f327109e68
Joshnee Kim Cunanan <joshnee.kim.cunanan@gmail.com>
2024-12-15 22:09:28 +0800

    fixed tailwindcss installation

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -6,24 +6,22 @@ import Button from '../components/button';
 
 
 export default function Home() {
-  const router = useRouter();
+    const router = useRouter();
 
-  const handleSubmit = () => {
-    router.push('/registration');
-    console.log('Form Submitted');
-  };
+    const handleSubmit = () => {
+        router.push('/registration');
+        console.log('Form Submitted');
+    };
 
-  return (
-    <div className="flex justify-center items-center h-screen bg-gray-100">
-      <p className="p-10 bg-green-500 text-white text-2xl">
-        Welcome to my Home Page! Please register here:
-      </p>
-      <Button
-        text="Register"
-        type="submit"
-        onClick={handleSubmit}
-        className='mt-4'
-      />
-    </div>
-  );
+    return (
+        <div className="flex justify-center items-center h-screen bg-gray-100">
+            <p className="p-10 bg-green-500 text-white text-2xl">
+                Welcome to my Home Page! Please register here:
+            </p>
+            <Button
+                text="Register"
+                onClick={handleSubmit}
+            />
+        </div>
+    );
 }
