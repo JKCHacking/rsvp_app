f7964a8bf294ef16ca1744becc82c6b0669345d7
Joshnee Kim Cunanan <joshnee.kim.cunanan@gmail.com>
2025-04-08 21:30:30 +0800

    persist SQLite db file in the host machine.

--- backend/database/database.go
+++ backend/database/database.go
@@ -1,17 +1,18 @@
 package database
 
 import (
+	"log"
+
 	"github.com/JKCHacking/rsvp_app/backend/models"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
-	"log"
 )
 
 var DB *gorm.DB
 
 func ConnectDatabase() {
 	var err error
-	DB, err = gorm.Open(sqlite.Open("rsvp.db"), &gorm.Config{})
+	DB, err = gorm.Open(sqlite.Open("/app/data/rsvp.db"), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal("Failed to connect to database:", err)
