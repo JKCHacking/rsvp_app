fc510010f6cacb0571b08b127662f15c9e1df80e
Joshnee Kim Cunanan <joshnee.kim.cunanan@gmail.com>
2025-04-11 23:36:20 +0800

    changed the text to black in login page

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -29,7 +29,7 @@ export default function Login() {
     }
 
     return (
-        <div className="min-h-screen flex items-center justify-center bg-gray-900 p-4 text-white">
+        <div className="min-h-screen flex items-center justify-center bg-gray-900 p-4">
         <div className="bg-gray-800 p-8 rounded-2xl shadow-lg w-full max-w-md">
             <h2 className="text-2xl font-bold text-center mb-6">Login</h2>
             <form className="space-y-5" onSubmit={handleLogin}>
